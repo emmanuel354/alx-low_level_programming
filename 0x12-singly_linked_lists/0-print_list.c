81b876a42e9d9461efdb3a29dd51aac55af70cc5
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-05-24 12:38:05 +0300

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	while(h)
-	{
 
-		if[5~[5~(!h->str)
+	while (h)
+	{
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
-	return(s);
+
+	return (s);
 }
